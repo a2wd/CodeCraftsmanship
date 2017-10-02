220b1b55a8074dee7740e4aef28444f202e2fb58
a2wd <github@a2wd.com>
2017-10-02 17:20:59 +0100

    Added week 1, session 1 files - initial fizzbuzz work

---
+++ Week1.FizzBuzz.Tests/FizzBuzzLogicShould.cs
@@ -0,0 +1,72 @@
+﻿namespace Week1.FizzBuzz.Tests
+{
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FizzBuzzLogicShould
+    {
+        private FizzBuzzLogic _fizzBuzzLogic;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fizzBuzzLogic = new FizzBuzzLogic();
+        }
+
+        [Test]
+        public void Return100Items()
+        {
+           string[] results;
+
+           results = _fizzBuzzLogic.GetFizzBuzz();
+
+           Assert.That(results.Length == 100);
+        }
+
+        [Test]
+        public void Return100StringItems()
+        {
+            string[] results;
+
+            results = _fizzBuzzLogic.GetFizzBuzz();
+
+            Assert.That(results.All(result => result.GetType() == typeof(string)), Is.True);
+        }
+
+        [Test]
+        public void Returns100NonEmptyItems()
+        {
+            string[] results;
+
+            results = _fizzBuzzLogic.GetFizzBuzz();
+
+            Assert.That(results.All(string.IsNullOrWhiteSpace), Is.False);
+        }
+
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(4, "4")]
+        [TestCase(5, "Buzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(7, "7")]
+        [TestCase(8, "8")]
+        [TestCase(9, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(11, "11")]
+        [TestCase(12, "Fizz")]
+        [TestCase(13, "13")]
+        [TestCase(14, "14")]
+        [TestCase(15, "FizzBuzz")]
+        public void ReturnTheCorrectValueAtCorrespondingIndices(int indice, string value)
+        {
+            string[] results;
+            int arrayIndex = indice - 1;
+
+            results = _fizzBuzzLogic.GetFizzBuzz();
+
+            Assert.That(results[arrayIndex], Is.EqualTo(value));
+        }
+    }
+}
---
+++ Week1.FizzBuzz/FizzBuzzLogic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1.FizzBuzz
+{
+    public class FizzBuzzLogic
+    {
+        private const string FizzText = "Fizz";
+
+        private const string BuzzText = "Buzz";
+
+        private const string FizzBuzzText = "FizzBuzz";
+
+        public string[] GetFizzBuzz()
+        {
+            var fizzBuzzValues = new string[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                fizzBuzzValues[i] =  CalculateFizzBuzzValue(i);
+            }
+           
+            return fizzBuzzValues;
+        }
+
+        private string CalculateFizzBuzzValue(int indice)
+        {
+            var outputValue = indice + 1;
+
+            if (outputValue % 15 == 0)
+            {
+                return FizzBuzzText;
+            }
+
+            if (outputValue % 5 == 0)
+            {
+                return BuzzText;
+            }
+
+            if (outputValue % 3 == 0)
+            {
+                return FizzText;
+            }
+
+            return outputValue.ToString();
+        }
+    }
+}
