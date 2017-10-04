b33825444651e437ba26278ad7f7ace804e90ee6
a2wd <github@a2wd.com>
2017-10-04 17:15:57 +0100

    Added work on string calculator

---
+++ Week1.StringCalculator.Tests/StringCalculatorLogicShould.cs
@@ -0,0 +1,53 @@
+﻿namespace Week1.StringCalculator.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringCalculatorLogicShould
+    {
+        private StringCalculatorLogic _stringCalculatorLogic;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stringCalculatorLogic = new StringCalculatorLogic();
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase(" ")]
+        public void Return0WhenPassed0Numbers(string input)
+        {
+            int result = _stringCalculatorLogic.Add(input);
+
+            Assert.That(result, Is.Zero);
+        }
+
+        [TestCase("test")]
+        [TestCase("1,bob")]
+        [TestCase("bob,2")]
+        public void ThrowAnInvalidInputExceptionWhenNotPassedIntegers(string input)
+        {
+            Assert.Throws(typeof(InvalidInputException), () => _stringCalculatorLogic.Add(input));
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("0", 0)]
+        [TestCase("10", 10)]
+        [TestCase("99", 99)]
+        public void ReturnANumberWhenPassedAnIntegerAsAString(string input, int expectedOutput)
+        {
+            int result = _stringCalculatorLogic.Add(input);
+
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+
+        [TestCase("1,2")]
+        [TestCase("15,24")]
+        public void ShouldNotThrowAnExceptionOnPassingTwoIntegers(string input)
+        {
+            Assert.DoesNotThrow(() => _stringCalculatorLogic.Add(input));
+        }
+    }
+}
---
+++ Week1.StringCalculator/StringCalculatorLogic.cs
@@ -0,0 +1,32 @@
+﻿namespace Week1.StringCalculator
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StringCalculatorLogic
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            var outputNumbersAsString = numbers.Split(',');
+
+            int[] outputNumbers = new int[outputNumbersAsString.Length];
+
+            for (int i = 0; i < outputNumbers.Length; i++)
+            {
+                if (int.TryParse(outputNumbersAsString[i], out outputNumbers[i]) == false)
+                {
+                    throw new InvalidInputException();
+                }
+            }
+
+            return outputNumbers.Sum();
+        }
+    }
+}
