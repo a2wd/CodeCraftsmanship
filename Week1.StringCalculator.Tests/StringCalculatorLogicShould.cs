4c956294b1fa56f5f734a5b16279643c346fb75c
a2wd <github@a2wd.com>
2017-10-05 14:37:42 +0100

    Fixed naming issue in test, added test to check sum & test to check newlines within input numbers

--- Week1.StringCalculator.Tests/StringCalculatorLogicShould.cs
+++ Week1.StringCalculator.Tests/StringCalculatorLogicShould.cs
@@ -45,9 +45,30 @@
 
         [TestCase("1,2")]
         [TestCase("15,24")]
-        public void ShouldNotThrowAnExceptionOnPassingTwoIntegers(string input)
+        public void NotThrowAnExceptionOnPassingTwoIntegers(string input)
         {
             Assert.DoesNotThrow(() => _stringCalculatorLogic.Add(input));
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("9,5", 14)]
+        [TestCase("11,29", 40)]
+        [TestCase("230,431", 661)]
+        public void CorrectlySumTwoIntegers(string input, int expectedOutput)
+        {
+            int result = _stringCalculatorLogic.Add(input);
+
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+
+        [TestCase("6\n0,32", 92)]
+        [TestCase("12,4\n8", 60)]
+        [TestCase("3\n12,8\n8\n3", 1195)]
+        public void AllowNewLinesWithinInputNumbers(string input, int expectedOutput)
+        {
+            int result = _stringCalculatorLogic.Add(input);
+
+            Assert.That(result, Is.EqualTo(expectedOutput));
         }
     }
 }
