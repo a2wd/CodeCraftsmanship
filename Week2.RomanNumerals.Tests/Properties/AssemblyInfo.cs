1a2c736b2137c678803edfae03d3f01d374cb251
Inderpal <inderpal.ghataore@asos.com>
2017-10-10 17:16:09 +0100

    add week 2 roman numeral

---
+++ Week2.RomanNumerals.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week2.RomanNumerals.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ASOS.com")]
+[assembly: AssemblyProduct("Week2.RomanNumerals.Tests")]
+[assembly: AssemblyCopyright("Copyright © ASOS.com 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0afd567c-51d1-4f1c-9ce0-9cc75b9f7724")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week2.RomanNumerals.Tests/RomanNumeralsConverterShould.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Week2.RomanNumerals.Tests
+{
+    [TestFixture]
+    public class RomanNumeralsConverterShould
+    {
+        [Test]
+        public void ReturnAStringGivenAPositiveInteger()
+        {
+            var result = new RomanNumeralsConverter().Convert(1);
+
+            Assert.That(result.GetType(),Is.EqualTo(typeof(string)));
+        }
+
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        [TestCase(12, "XII")]
+        [TestCase(13, "XIII")]
+        [TestCase(14, "XIV")]
+        [TestCase(15, "XV")]
+        [TestCase(16, "XVI")]
+        [TestCase(17, "XVII")]
+        [TestCase(18, "XVIII")]
+        [TestCase(19, "XIX")]
+        [TestCase(20, "XX")]
+        [TestCase(40, "XL")]
+        public void ReturnARomanNumeralGivenAnArabicNumeral(int arabicNumeral, string expectedOutput)
+        {
+            var result = new RomanNumeralsConverter().Convert(arabicNumeral);
+
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+    }
+
+    public class RomanNumeralsConverter
+    {
+        private static readonly Dictionary<int, string> ArabicNumeralsToRomanNumerals = new Dictionary<int, string>()
+        {
+            {1, "I" },
+            {4, "IV" },
+            {5, "V" },
+            {9, "IX" },
+            {10, "X" },
+            {40, "XL" }
+        };
+
+        public string Convert(int arabicNumber)
+        {
+            if (ArabicNumeralsToRomanNumerals.ContainsKey(arabicNumber))
+            {
+                return ArabicNumeralsToRomanNumerals[arabicNumber];
+            }
+
+            if (arabicNumber > 10)
+            {
+                return ArabicNumeralsToRomanNumerals[10] + Convert(arabicNumber - 10);
+            }
+
+            if (arabicNumber > 5)
+            {
+               return ArabicNumeralsToRomanNumerals[5] + Convert(arabicNumber - 5);
+            }
+            
+            return ArabicNumeralsToRomanNumerals[1] + Convert(arabicNumber - 1);
+        }
+    }
+}
