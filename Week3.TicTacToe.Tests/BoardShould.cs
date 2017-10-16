c1613dc5fff6483caffc0fc021cc2535cf374d99
a2wd <github@a2wd.com>
2017-10-16 17:06:49 +0100

    Initial work on Tic-Tac-Toe kata

---
+++ Week3.TicTacToe.Tests/BoardShould.cs
@@ -0,0 +1,52 @@
+﻿namespace Week3.TicTacToe.Tests
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BoardShould
+    {
+        [Test]
+        public void ReturnABoardObject()
+        {
+            var result = new Board();
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void InitialiseA3By3BoardWithEmptyCells()
+        {
+            var board = new Board();
+
+            for (int cellIndex = 0; cellIndex < 9; cellIndex++)
+            {
+                var column = cellIndex % 3;
+                var row = column / 3;
+
+                var cellIsEmpty = board.IsCellEmpty(row, column);
+
+                Assert.That(cellIsEmpty, Is.True);
+            }
+        }
+
+        [Test]
+        public void BeAbleToSetValueOfACell()
+        {
+            var board = new Board();
+
+            board.SetCellToX(1, 2);
+
+            Assert.That(board.IsCellEmpty(1,2), Is.False);
+        }
+
+        [Test]
+        public void ThrowAnErrorWhenAnInvalidCellIsRead()
+        {
+            var board = new Board();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => board.IsCellEmpty(5, 5));
+        }
+    }
+}
---
+++ Week3.TicTacToe/Board.cs
@@ -0,0 +1,33 @@
+﻿namespace Week3.TicTacToe
+{
+    using System;
+
+    public class Board
+    {
+        private int ValueWhenEmpty = 0;
+
+        private int ValueOfX = 1;
+
+        private int[,] Cells;
+
+        public Board()
+        {
+            Cells = new int[3,3];
+        }
+
+        public bool IsCellEmpty(int row, int column)
+        {
+            if (row > 3 || column > 3)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return Cells[row, column] == ValueWhenEmpty;
+        }
+
+        public void SetCellToX(int row, int column)
+        {
+            Cells[row, column] = ValueOfX;
+        }
+    }
+}
