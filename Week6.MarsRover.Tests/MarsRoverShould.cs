71afc52bd4ff3d53f2006aa5ef8f9ea276199b4e
a2wd <github@a2wd.com>
2017-11-20 16:55:00 +0000

    Implemented turning and moving

--- Week6.MarsRover.Tests/MarsRoverShould.cs
+++ Week6.MarsRover.Tests/MarsRoverShould.cs
@@ -20,12 +20,94 @@
         }
 
         [Test]
-        public void ThrowExceptionWhenRoverLandsOutsidePlateuBoundary()
+        public void ThrowExceptionWhenRoverLandsOutsidePlateauBoundary()
         {
             var initialPosition = new VectorWithHeading(6, 6, Heading.North);
             var plateau = new Plateau(5, 5);
 
            Assert.Throws<RoverOutsideOfPlateauException>(() => new MarsRover(initialPosition, plateau));
+        }
+
+        [Test]
+        public void BeOrientedToTheSouthWhenAfterTurningTwiceFromAStartingOrientationOfNorth()
+        {
+            var initialPosition = new VectorWithHeading(2, 2, Heading.North);
+            var plateau = new Plateau(5, 5);
+
+            var marsRover = new MarsRover(initialPosition, plateau);
+
+            marsRover.TurnRight();
+            marsRover.TurnRight();
+
+            var result = marsRover.GetVectorWithHeading();
+
+            Assert.That(result, Is.EqualTo(new VectorWithHeading(2, 2, Heading.South)));
+        }
+
+        [Test]
+        public void BeOrientedToTheWestWhenAfterTurningOnceFromAStartingOrientationOfNorth()
+        {
+            var initialPosition = new VectorWithHeading(2, 2, Heading.North);
+            var plateau = new Plateau(5, 5);
+
+            var marsRover = new MarsRover(initialPosition, plateau);
+
+            marsRover.TurnLeft();
+
+            var result = marsRover.GetVectorWithHeading();
+
+            Assert.That(result, Is.EqualTo(new VectorWithHeading(2, 2, Heading.West)));
+        }
+
+        [Test]
+        public void MovePositionByOneFromInitialPositionWhenMoveCommandCalled()
+        {
+            var initialPosition = new VectorWithHeading(2, 2, Heading.North);
+            var plateau = new Plateau(5, 5);
+
+            var marsRover = new MarsRover(initialPosition, plateau);
+
+            marsRover.Move();
+
+            var result = marsRover.GetVectorWithHeading();
+
+            Assert.That(result, Is.EqualTo(new VectorWithHeading(2, 3, Heading.North)));
+        }
+
+        [Test]
+        public void ThrowARoverOutsideOfPlateauExceptionWhenMovedOutsideOfThePlateauBounds()
+        {
+            var initialPosition = new VectorWithHeading(2, 2, Heading.North);
+            var plateau = new Plateau(2, 2);
+
+            var marsRover = new MarsRover(initialPosition, plateau);
+
+            Assert.Throws<RoverOutsideOfPlateauException>(() => marsRover.Move());
+        }
+
+        [Test]
+        public void ReturnExpectedOutputOnSetSequenceofMovementCommands()
+        {
+            var initialPosition = new VectorWithHeading(1, 2, Heading.North);
+            var expectedPosition = new VectorWithHeading(1, 3, Heading.North);
+
+            var plateau = new Plateau(5, 5);
+            
+            var marsRover = new MarsRover(initialPosition, plateau);
+
+            marsRover.TurnLeft();
+            marsRover.Move();
+            marsRover.TurnLeft();
+            marsRover.Move();
+            marsRover.TurnLeft();
+            marsRover.Move();
+            marsRover.TurnLeft();
+            marsRover.Move();
+            marsRover.Move();
+
+            var result = marsRover.GetVectorWithHeading();
+
+            Assert.That(result, Is.EqualTo(expectedPosition));
         }
     }
 }
--- Week6.MarsRover/MarsRover.cs
+++ Week6.MarsRover/MarsRover.cs
@@ -5,7 +5,7 @@
 
     public class MarsRover
     {
-        private readonly VectorWithHeading _currentVectorWithHeading;
+        private VectorWithHeading _currentVectorWithHeading;
         private readonly Plateau _plateau;
 
         public MarsRover(VectorWithHeading initialVectorWithHeading, Plateau plateu)
@@ -13,18 +13,18 @@
             _plateau = plateu;
             _currentVectorWithHeading = initialVectorWithHeading;
 
-            if (InitialVectorIsOutsidePlateu())
+            if (IsVectorIsOutsidePlateau(initialVectorWithHeading))
             {
                 throw new RoverOutsideOfPlateauException();
             }
         }
 
-        private bool InitialVectorIsOutsidePlateu()
+        private bool IsVectorIsOutsidePlateau(VectorWithHeading vectorWithHeading)
         {
-            if (_currentVectorWithHeading.GetXPosition() < 0 ||
-                _currentVectorWithHeading.GetYPosition() < 0 ||
-                _currentVectorWithHeading.GetXPosition() > _plateau.GetXPosition() ||
-                _currentVectorWithHeading.GetYPosition() > _plateau.GetYPosition())
+            if (vectorWithHeading.GetXPosition() < 0 ||
+                vectorWithHeading.GetYPosition() < 0 ||
+                vectorWithHeading.GetXPosition() > _plateau.GetXPosition() ||
+                vectorWithHeading.GetYPosition() > _plateau.GetYPosition())
             {
                 return true;
             }
@@ -36,5 +36,89 @@
         {
             return _currentVectorWithHeading;
         }
+
+        public void TurnRight()
+        {
+            var newHeading = Heading.North;
+
+            switch (GetVectorWithHeading().GetHeading())
+            {
+                case Heading.North:
+                    newHeading = Heading.East;
+                    break;
+                case Heading.East:
+                    newHeading = Heading.South;
+                    break;
+                case Heading.South:
+                    newHeading = Heading.West;
+                    break;
+                case Heading.West:
+                    newHeading = Heading.North;
+                    break;
+            }
+
+            _currentVectorWithHeading = new VectorWithHeading(
+                GetVectorWithHeading().GetXPosition(),
+                GetVectorWithHeading().GetYPosition(),
+                newHeading);
+        }
+
+        public void TurnLeft()
+        {
+            var newHeading = Heading.North;
+
+            switch (GetVectorWithHeading().GetHeading())
+            {
+                case Heading.North:
+                    newHeading = Heading.West;
+                    break;
+                case Heading.East:
+                    newHeading = Heading.North;
+                    break;
+                case Heading.South:
+                    newHeading = Heading.East;
+                    break;
+                case Heading.West:
+                    newHeading = Heading.South;
+                    break;
+            }
+
+            _currentVectorWithHeading = new VectorWithHeading(
+                GetVectorWithHeading().GetXPosition(),
+                GetVectorWithHeading().GetYPosition(),
+                newHeading);
+        }
+
+        public void Move()
+        {
+            int xPosition = GetVectorWithHeading().GetXPosition();
+            int yPosition = GetVectorWithHeading().GetYPosition();
+
+            var heading = GetVectorWithHeading().GetHeading();
+
+            switch (heading)
+            {
+                case Heading.North:
+                    yPosition++;
+                    break;
+                case Heading.East:
+                    xPosition++;
+                    break;
+                case Heading.South:
+                    yPosition--;
+                    break;
+                case Heading.West:
+                    xPosition--;
+                    break;
+            }
+            
+            _currentVectorWithHeading = new VectorWithHeading(xPosition, yPosition, heading);
+
+            if (IsVectorIsOutsidePlateau(_currentVectorWithHeading))
+            {
+                throw new  RoverOutsideOfPlateauException();
+            }
+        }
     }
 }
+
