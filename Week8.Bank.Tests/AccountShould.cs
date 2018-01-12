51ae2d6633a2b0065e5c1b9565a0e9cdf1562bfb
a2wd <github@a2wd.com>
2018-01-12 16:25:13 +0000

    Added tests for account

--- Week8.Bank.Tests/AccountShould.cs
+++ Week8.Bank.Tests/AccountShould.cs
@@ -1,7 +1,39 @@
 ﻿namespace Week8.Bank.Tests
 {
+    using System;
+    using System.Linq;
+    using Account;
+    using NUnit.Framework;
+    using Transaction;
+
+    [TestFixture]
     public class AccountShould
     {
-        
+        private IAccount _account;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _account = new Account();
+        }
+
+        [Test]
+        public void ReturnAnEmptyListOfTransactionsWhenNoTransactionHaveBeenAdded()
+        {
+            var transactionList = _account.GetTransactions();
+
+            Assert.That(transactionList, Is.Not.Null);
+        }
+
+        [Test]
+        public void AllowATransactionToBeAdded()
+        {
+            _account.AddTransaction(new Transaction(0m, DateTime.UtcNow, 0m));
+
+            var transactionList = _account.GetTransactions();
+
+            Assert.That(transactionList, Is.Not.Null);
+            Assert.That(transactionList.Count(), Is.EqualTo(1));
+        }
     }
 }
---
+++ Week8.Bank/Account/Account.cs
@@ -0,0 +1,31 @@
+﻿namespace Week8.Bank.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using Transaction;
+
+    public class Account : IAccount
+    {
+        private readonly List<ITransaction> _transactions;
+
+        public Account()
+        {
+            _transactions = new List<ITransaction>();
+        }
+
+        public void AddTransaction(ITransaction transaction)
+        {
+            _transactions.Add(transaction);
+        }
+
+        public IEnumerable<ITransaction> GetTransactions()
+        {
+            return _transactions;
+        }
+
+        public decimal GetBalance()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
